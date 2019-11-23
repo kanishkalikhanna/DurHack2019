2a52197580f0f2416276830f489f9fffb16b5f82
kanishkalikhanna <kanishkalikhanna@gmail.com>
2019-11-23 13:23:32 +0000

    added month parameter to dataste

--- creating_dataset.py
+++ creating_dataset.py
@@ -1,8 +1,21 @@
-print("Hello, Durham")
-
 import random
-def data_generator (file_name):
+def data_generator (file_name, number_of_consumers):
+
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     consumer_spending = open(file_name, "w")
 
-data_generator("consumer_spending.csv")
+    consumer_spending.write("ID,MONTH,INCOME,FOOD EXPENDITURE, ACCOMODATION EXPENDITURE, LEISURE EXPENDITURE")
 
+    for i in range (1, number_of_consumers + 1):
+
+        income = random.randint(1000, 4000)
+
+        for j in months:
+
+            perc_leisure = random.randint(5, 50)/100
+            perc_accomodation = random.randint(20,50)/100
+            perc_food = random.randint(10,20)/100
+
+            consumer_spending.write("\n" + str(i) + "," + j +  "," + str(income) + "," + str(round(perc_food*income, 2)) + "," + str(round(perc_accomodation*income,2)) + "," + str(round(perc_leisure*income,2)))
+
+data_generator("consumer_spending.csv", 5)
