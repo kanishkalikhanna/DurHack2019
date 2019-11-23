260119acbc61475490e4efd356ba1f3a79010e8e
Mohamed <hammeda@DESKTOP-9HM0F6G.localdomain>
2019-11-23 22:32:22 +0000

    Additions to tempMain

--- tempMain.py
+++ tempMain.py
@@ -1,11 +1,27 @@
-import creating_dataset
+import transaction_classifier
 import target_spend_generator
 
 accounts = {}
 transactions = {}
+accounts_smart_scores = {}
+accounts_overall_smart_score = {}
 
-creating_dataset.data_generator(people)
+target_spend_generator.scan_csv("consumer_spending", accounts)
+transaction_classifier.scan_csv("", transactions, accounts_smart_scores)
+
+for account_smart_scores in accounts_smart_scores:
+
+    count = 0
+    total_smart_score = 0
+
+    for smart_score in accounts_smart_scores[account_smart_scores]:
+        count += 1
+        total_smart_score += smart_score
+
+    account_overall_smart_score = total_smart_score / count
+
+    accounts_overall_smart_score[account_smart_scores] = account_overall_smart_score
 
 
-
+for transaction in transactions:
 
--- transaction_classifier.py
+++ transaction_classifier.py
@@ -1,43 +1,41 @@
 # Author: Mohamed Hammeda
 import csv
-import statistics
+
 
 class Transaction:
 
-    def __init__(self, transaction_id, account_id, amount, necessity, people):
+    def __init__(self, transaction_id, account_id, amount, necessity, accounts, group):
 
         self.id = transaction_id
         self.amount = amount
-        self.accountId = account_id
+        self.account_id = account_id
         self.necessity = necessity
-        self.ideal_smart_score = calculateSmartScore(people, account_id, amount, necessity)
 
-
-    def calculateSmartScore(self, accounts, account_id, amount, necessity, group):
         account = accounts[account_id]
         multiplier = 100 / (necessity + 1)
+        fraction = 0
+
+        if group == "grocery" or account == "fine dining" or account == "fast food":
+            fraction = (10 * amount) / account.get_ideal_food_expenditure()
+
+        elif group == "rent" or group == "utility bills":
+            fraction = (10 * amount) / account.get_ideal_accommodation_expenditure()
+
+        elif group == "travelling" or group == "gym" or group == "movies":
+            fraction = (10 * amount) / account.get_ideal_leisure_expenditure()
+
+        smart_score = multiplier * fraction
+
+        self.smart_score = smart_score
+
+    def get_account_id(self):
+        return self.account_id
+
+    def get_smart_sore(self):
+        return self.smart_score
 
 
-        if group == "":
-            fraction = (10 * amount) /
-
-        elif group == "":
-            fraction = (10 * amount) /
-
-        elif group == "":
-            fraction = (10 * amount) /
-
-        elif group == "":
-            fraction = (10 * amount) /
-
-        elif group == "":
-            fraction = (10 * amount) /
-
-        smartScore = multiplier * fraction
-
-        return smartScore
-
-def scan_csv(file_name, transactions):
+def scan_csv(file_name, transactions, accounts_smart_scores):
 
     with open(file_name) as file:
 
@@ -48,4 +46,11 @@ def scan_csv(file_name, transactions):
         for row in reader:
 
             transactions[transaction_id] = Transaction(transaction_id, account_id, amount, necessity)
+
+            if account_id in accounts_smart_scores:
+                accounts_smart_scores[account_id].append(transactions[transaction_id].get_smart_score)
+            else:
+                accounts_smart_scores[account_id] = [transactions[transaction_id].get_smart_score]
+
+
 
