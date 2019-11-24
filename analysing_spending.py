55989df5ff2f25cc44fcc2684e987bd13f32ff27
kanishkalikhanna <kanishkalikhanna@gmail.com>
2019-11-24 09:31:29 +0000

    added ability to create pdf of graphs

--- analysing_spending.py
+++ analysing_spending.py
@@ -1,6 +1,8 @@
 import csv
 from matplotlib.pylab import *
 import matplotlib.style as style
+from matplotlib.backends.backend_pdf import PdfPages
+
 
 style.use('bmh')
 style.use('grayscale')
@@ -14,6 +16,7 @@ accom_exp = []
 leisure_exp = []
 id_expenditure_plot_map = {}
 id_savings_plot_map = {}
+figures = []
 
 months_ordered = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
                   "November", "December"]
@@ -35,19 +38,15 @@ except FileNotFoundError:
     sys.exit(0)
 
 def plot_expenditure_for_each_person(person_id):
-    income_for_person = []
-    sum_each_month = []
-    for person in range(len(user_id)):
-        if user_id[person] == person_id:
-            income_for_person.append(income[person])
-            sum_each_month.append(food_exp[person] + accom_exp[person] + leisure_exp[person])
 
+    income_for_person, sum_each_month = sum_different_expenditures(person_id)
     y_min = min(sum_each_month)
     x_pos = sum_each_month.index(y_min)
     x_min = months_ordered[x_pos]
+    fig = plt.figure()
     plt.annotate(round(y_min, 2), xy=(x_min, y_min), xytext=(x_min, y_min + 5),
-                 arrowprops=dict(facecolor='red', shrink=0.05), )
-    barplot = plt.bar(months_ordered, sum_each_month, color='cadetblue')
+                 arrowprops=dict(facecolor='red', shrink=0.05))
+    barplot = plt.bar(months_ordered, sum_each_month, color='cadetblue', figure = fig)
     barplot[x_pos].set_color('aqua')
     plt.plot(months_ordered, income_for_person, label="Income")
     plt.legend(loc="upper left")
@@ -55,18 +54,21 @@ def plot_expenditure_for_each_person(person_id):
     plt.xticks(rotation=30)
     ylabel("£", rotation=0)
     xlabel("Months")
+    title("Monthly Total Expenditure and Income")
+    figures.append(fig)
     plt.show()
-
 
 def plot_savings_for_each_person(person_id):
     income_for_person, sum_each_month = sum_different_expenditures(person_id)
     savings = []
     for j in range(len(income_for_person)):
         savings.append(((income_for_person[j] - sum_each_month[j]) / income_for_person[j]) * 100)
-    plt.plot(months_ordered, savings)
+    fig = plt.figure()
+    plt.plot(months_ordered, savings, figure = fig)
     plt.xticks(rotation=30)
     ylabel("%", rotation = 0)
     title("Savings as % of Income")
+    figures.append(fig)
     plt.show()
 
 def sum_different_expenditures(person_id):
@@ -78,6 +80,12 @@ def sum_different_expenditures(person_id):
             sum_each_month.append(food_exp[person] + accom_exp[person] + leisure_exp[person])
     return income_for_person, sum_each_month
 
+
 for i in range(1, len(set(user_id)) + 1):
     plot_expenditure_for_each_person(i)
     plot_savings_for_each_person(i)
+
+pdf = PdfPages("graphs.pdf")
+for fig in figures:
+    fig.savefig(pdf, format = 'pdf')
+pdf.close()
